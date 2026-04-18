06d46b5c16e7dd204f9a340ee5e0d81fac508c4c
FranCampora <camporafran@gmail.com>
2026-04-18 11:37:57 -0300

    Limpio repo respetando git ignore

--- .Trash-labo3/def (otra copia).h
+++
@@ -1,9 +0,0 @@
-#ifndef _DEF_H
-#define _DEF_H
-
-#define TRUE 1
-#define FALSE 0
-#define DESDE 0
-#define HASTA 50
-
-#endif
--- .Trash-labo3/funciones (otra copia).h
+++
@@ -1,5 +0,0 @@
-#ifndef _FUNCIONES_H
-#define _FUNCIONES_H
-
-	int idNumeroAleatorio(int desde, int hasta);
-#endif
